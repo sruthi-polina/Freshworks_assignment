911f4427f94e09b01663f6e8cff7059ef8bd4b5e
sruthi-polina <65865751+sruthi-polina@users.noreply.github.com>
2020-12-31 16:45:42 +0530

    Add files via upload
    CRD operations for file based key-value datastore

---
+++ code.py
@@ -0,0 +1,62 @@
+import threading 
+#this is for python 3.0 and above. use import thread for python2.0 versions
+from threading import*
+import time
+
+d={} #dictionary to  store data
+
+#create operation 
+#use syntax "create(key_name,value,timeout_value)" timeout is optional 
+
+def create(key,value,timeout=0):
+    if key in d:
+        print("error: this key already exists") 
+    else:
+        if(key.isalpha()):
+            if len(d)<(1024*1020*1024) and value<=(16*1024*1024): #constraints for file size less than 1GB and Jasonobject value less than 16KB 
+                if timeout==0:
+                    l=[value,timeout]
+                else:
+                    l=[value,time.time()+timeout]
+                if len(key)<=32: #constraints for input key_name capped at 32chars
+                    d[key]=l
+            else:
+                print("error: Memory limit exceeded!! ")
+        else:
+            print("error: Invalind key_name!! key_name must contain only alphabets and no special characters or numbers")
+
+#read operation
+#use syntax "read(key_name)"
+            
+def read(key):
+    if key not in d:
+        print("error: given key does not exist in database. Please enter a valid key") 
+    else:
+        b=d[key]
+        if b[1]!=0:
+            if time.time()<b[1]: #comparing the present time with expiry time
+                stri=str(key)+":"+str(b[0]) #to return the value in the format of JasonObject i.e.,"key_name:value"
+                return stri
+            else:
+                print("error: time-to-live of",key,"has expired") 
+        else:
+            stri=str(key)+":"+str(b[0])
+            return stri
+
+# delete operation
+#syntax "delete(key_name)"
+
+def delete(key):
+    if key not in d:
+        print("error: given key does not exist in database. Please enter a valid key")
+    else:
+        b=d[key]
+        if b[1]!=0:
+            if time.time()<b[1]: #comparing the current time with expiry time
+                del d[key]
+                print("key is successfully deleted")
+            else:
+                print("error: time-to-live of",key,"has expired")
+        else:
+            del d[key]
+            print("key is successfully deleted")
