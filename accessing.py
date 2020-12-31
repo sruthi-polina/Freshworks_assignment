6f0bcdd514a44135366ac81850ffa91b8a7a7bfe
sruthi-polina <65865751+sruthi-polina@users.noreply.github.com>
2020-12-31 18:36:11 +0530

    Update accessing.py

--- accessing.py
+++ accessing.py
@@ -1,27 +1,18 @@
 #the commands to demonstrate how to access and perform operations on a main file
-
-
 #run the MODULE of MAIN FILE and import mainfile as a library 
-
 import code as x 
 #importing the main file("code") as a library 
-
-
 x.create("hii",12)
 #create a key with key_name,value and with no time-to-live property
-
 
 x.create("hello",50,1000) 
 #create a key with key_name,value  and with time-to-live property value given(number of seconds)
 
-
 x.read("hii")
 #it returns output in  Jasonobject format 'key_name:value'
 
-
 x.read("hello")
 #it returns output in Jasonobject format if the TIME-TO-LIVE IS NOT EXPIRED else it returns an ERROR
-
 
 x.create("h",50)
 #it returns an ERROR since the key_name already exists in the database
